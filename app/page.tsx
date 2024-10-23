ea63eb7e30a0ef6d58fbf1642bd90b4116362362
Hollie Duncan <hollie@contento.io>
2024-10-23 12:03:08 +0100

    Adds generateMetaData to app > page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-import { createClient } from '@/lib/contento'
+import { createClient, generateSeo } from '@/lib/contento'
+import { ContentData } from '@gocontento/client'
+import { Metadata } from 'next'
 import { draftMode } from 'next/headers'
 import { notFound } from 'next/navigation'
 
+export async function generateMetadata(): Promise<Metadata> {
+  return await createClient()
+    .getContentBySlug('home', 'general_page')
+    .then((content: ContentData) => {
+      return generateSeo(content, {}, content.url?.replace('/home', ''))
+    })
+    .catch(() => {
+      return {}
+    })
+}
 
 export default async function page() {
   const content = await createClient(draftMode().isEnabled)
